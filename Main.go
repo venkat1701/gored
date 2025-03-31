5eebb34c13ade7b2cb18f79be4102b98367e0e4c
venkat1701 <thejeastdev@gmail.com>
2025-03-31 12:59:00 +0530

    changed name

--- Main.go
+++ Main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// we check for some diagnostic information
 	// like the Go version and number of CPUs available
-	fmt.Printf("Starting optimized Key-Value cache with Go version %s\n", runtime.Version())
+	fmt.Printf("Starting Gored cache with Go version %s\n", runtime.Version())
 	fmt.Printf("System has %d CPUs\n", runtime.NumCPU())
 
 	// and then we start our resp server on port 7171

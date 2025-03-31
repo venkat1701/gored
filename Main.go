daf589ed1129756dd9aad173de226a040cd5880d
venkat1701 <thejeastdev@gmail.com>
2025-03-31 08:07:54 +0530

    altered main.go with more information about system

--- Main.go
+++ Main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // This is where we start the server
 func main() {
+	// we check for some diagnostic information
+	// like the Go version and number of CPUs available
+	fmt.Printf("Starting optimized Key-Value cache with Go version %s\n", runtime.Version())
+	fmt.Printf("System has %d CPUs\n", runtime.NumCPU())
+
+	// and then we start our resp server on port 7171
 	StartServer()
 }
